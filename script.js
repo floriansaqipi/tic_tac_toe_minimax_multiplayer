67514ea6318aff9b6f2ebe91bfc67cab25e1ea93
Festim <krasniqif937@gmail.com>
2024-01-09 21:33:37 +0100

    Added id for each box and deleted e.innerHTML  before if statement in forEach method

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let turn = "X";
 let isGameOver = false;
 
 boxes.forEach(e =>{
-    e.innerHTML = ""
+   
     e.addEventListener("click", ()=>{
         if(!isGameOver && e.innerHTML === ""){
             e.innerHTML = turn;
