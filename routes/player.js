50f9decf6f538afe7ada9380a6c8b49539ff5e6a
ArditGjyrevci <ardit.gjyrevci@student.uni-pr.edu>
2024-01-12 21:34:13 +0100

    Update of script file

--- routes/player.js
+++ routes/player.js
@@ -14,16 +14,23 @@ router.get("/index", (req, res, next) => {
 
 router.post('/', (req, res,next) => {
     
-    const symbol = req.body.symbol;
-    const whoPlaysFirst = req.body.whoPlaysFirst;
+  const symbol = req.body.symbol;
+  const whoPlaysFirst = req.body.whoPlaysFirst;
 
-    let isPcFirst=whoPlaysFirst==='Computer';
-    
-      res.render('index', {
-        symbolOfPlayer:symbol,
-        whoPlaysFirst:whoPlaysFirst,
-        isPcTurn:isPcFirst
-       });
-  });
+  let isPcFirst=whoPlaysFirst==='Computer';
+  let symbol1='';
+  if(symbol==="X"){
+
+    symbol1='O';
+  }else{
+    symbol1='X';
+  }
+    res.render('index', {
+      symbolOfPlayer:symbol,
+      symbolOfPlayer1:symbol1,
+      whoPlaysFirst:whoPlaysFirst,
+      isPcTurn:isPcFirst
+     });
+});
 
 module.exports = router
