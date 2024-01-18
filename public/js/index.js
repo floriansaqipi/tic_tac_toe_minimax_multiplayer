f781d212f1bd2810a2906850b00a7db8951a57da
Gentrit <gentikryeziu851@gmail.com>
2024-01-18 16:50:34 +0100

    delete-script-js
    Deleted script.js file since it has been replaced by board.js file, and also removed unnecessary comments from index.js

--- public/js/index.js
+++ public/js/index.js
@@ -3,15 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     symbols.forEach(function(symbol) {
         symbol.addEventListener('click', function() {
-            // Remove 'selected' class from all symbols
             symbols.forEach(function(sym) {
                 sym.classList.remove('selected');
             });
-
-            // Add 'selected' class to the clicked symbol
             this.classList.add('selected');
-
-            // Update the actual radio input value
             document.getElementById('symbol' + this.dataset.symbol).checked = true;
         });
     });
